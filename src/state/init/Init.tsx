e66f987f1748a0d0aaa021623d17e53e7de2f521
Eric Charles <eric@datalayer.io>
2022-04-05 15:57:02 +0200

    fix: init actions are async

--- src/state/init/Init.tsx
+++ src/state/init/Init.tsx
@@ -9,12 +9,12 @@ const Init: React.FC = () => {
     <>
       <h2>Init value: {init}</h2>
       <button
-        onClick={() => dispatch(initActions.increment(undefined))}
+        onClick={() => dispatch(initActions.increment.started())}
       >
         Increment Init (1 second delay)
       </button>
       <button
-        onClick={() => dispatch(initActions.decrement(undefined))}
+        onClick={() => dispatch(initActions.decrement.started())}
       >
         Decrement Init (0.2 second delay)
       </button>
