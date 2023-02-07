061cbdc5e7446a55c119a3ba0af9c41f12940542
Eric Charles <eric@datalayer.io>
2023-02-07 13:53:24 +0100

    fix: handler

--- packages/react/src/jupyter/lab/handler.ts
+++ packages/react/src/jupyter/lab/handler.ts
@@ -17,7 +17,7 @@ export async function requestAPI<T>(
   const settings = ServerConnection.makeSettings();
   const requestUrl = URLExt.join(
     settings.baseUrl,
-    'datalayer', // API Namespace
+    'jupyter_react', // API Namespace
     endPoint
   );
 
