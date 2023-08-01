e8e2d60ca4793753225cb0b6fee002e828c69a0f
Eric Charles <eric@datalayer.io>
2023-08-01 19:10:53 +0200

    example: more ipywidgets examples

---
+++ packages/react/src/examples/Bqplot.tsx
@@ -0,0 +1,33 @@
+import { createRoot } from 'react-dom/client';
+import Jupyter from '../jupyter/Jupyter';
+import Notebook from '../components/notebook/Notebook';
+import NotebookToolbar from "./toolbars/NotebookToolbar";
+import CellSidebarDefault from "../components/notebook/cell/sidebar/CellSidebarDefault";
+
+import "./../../style/index.css";
+
+const NotebookExample = () => (
+  <Jupyter
+    lite={false}
+    useRunningKernelIndex={-1}
+    startDefaultKernel={true}
+    terminals={false}
+  >
+    <Notebook
+      path="bqplot.ipynb"
+      CellSidebar={CellSidebarDefault}
+      Toolbar={NotebookToolbar}
+      height='calc(100vh - 2.6rem)' // (Height - Toolbar Height).
+      cellSidebarMargin={120}
+      uid="notebook-uid"
+    />
+  </Jupyter>
+)
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+const root = createRoot(div)
+
+root.render(
+  <NotebookExample/>
+);
---
+++ packages/react/src/examples/IPyWidgets.tsx
@@ -0,0 +1,36 @@
+import { createRoot } from 'react-dom/client';
+import { INotebookContent } from '@jupyterlab/nbformat';
+import Jupyter from '../jupyter/Jupyter';
+import Notebook from '../components/notebook/Notebook';
+import NotebookToolbar from "./toolbars/NotebookToolbar";
+import CellSidebarDefault from "../components/notebook/cell/sidebar/CellSidebarDefault";
+
+import notebookExample from "./notebooks/IpyWidgetsExample1.ipynb.json";
+
+import "./../../style/index.css";
+
+const IPyWidgets = () => (
+  <Jupyter
+    lite={false}
+    useRunningKernelIndex={-1}
+    startDefaultKernel={true}
+    terminals={false}
+  >
+    <Notebook
+      nbformat={notebookExample as INotebookContent}
+      CellSidebar={CellSidebarDefault}
+      Toolbar={NotebookToolbar} 
+      height='calc(100vh - 2.6rem)' // (Height - Toolbar Height).
+      cellSidebarMargin={120}
+      uid="notebook-uid"
+    />
+  </Jupyter>
+)
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+const root = createRoot(div)
+
+root.render(
+  <IPyWidgets/>
+);
