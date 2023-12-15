8441b698756c41c21bde987aa8f1b5572ff947b3
echarles <eric@datalayer.io>
2023-12-15 20:16:11 +0100

    chore: remove theme package

--- packages/theme/src/css.d.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2022-2023 Datalayer Inc. All rights reserved.
- *
- * MIT License
- */
-
-declare module '*.css';
--- packages/theme/src/greeter.tsx
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2022-2023 Datalayer Inc. All rights reserved.
- *
- * MIT License
- */
-
-export const Greeter = (name: string) => `Hello ${name}`;
