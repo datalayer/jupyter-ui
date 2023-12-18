07e50028aac062de4720f4cb9d3e659e0205ab4d
echarles <eric@datalayer.io>
2023-12-18 17:37:47 +0100

    chore: kill script

--- dev/sh/kill.sh
+++ dev/sh/kill.sh
@@ -7,8 +7,22 @@
 # Copyright (c) Datalayer, Inc. https://datalayer.io
 # Distributed under the terms of the MIT License.
 
+uname_out="$(uname -s)"
+
+case "${uname_out}" in
+    Linux*)     export OS=LINUX;;
+    Darwin*)    export OS=MACOS;;
+#    CYGWIN*)    OS=CYGWIND;;
+#    MINGW*)     OS=MINGW;;
+    *)          export OS="UNSUPPORTED:${unameOut}"
+esac
+
 function kill_port() {
-    lsof -i TCP:$1 | grep LISTEN | awk '{print $2}' | xargs kill -9
+    case "${OS}" in
+        LINUX)     fuser -k $1/tcp;;
+        MACOS)     lsof -i TCP:$1 | grep LISTEN | awk '{print $2}' | xargs kill -9;;
+        *)         echo "Unsupported operating system ${OS}"
+    esac    
 }
 
 kill_port 3208
