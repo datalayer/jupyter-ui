4becf28f9fa0c5ca1f23edba52deb9b25f38b007
echarles <eric@datalayer.io>
2023-12-26 09:44:23 +0100

    typedoc: use version 0.8.1

--- packages/react/src/app/tabs/components/ViewerComponent.tsx
+++ packages/react/src/app/tabs/components/ViewerComponent.tsx
@@ -6,7 +6,7 @@
 
 import Viewer from '../../../components/viewer/Viewer';
 
-import nbformat from "../../..//examples/notebooks/IPyWidgetsExample1.ipynb.json";
+import nbformat from "../../..//examples/notebooks/IPyWidgetsExample.ipynb.json";
 
 const ViewerComponent = () => {
   return (
