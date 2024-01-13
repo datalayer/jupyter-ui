31fa959e16203a321f1ff1a2dc6c543b3a3686c5
echarles <eric@datalayer.io>
2024-01-13 09:46:50 +0100

    fix: webpack for jlab

--- packages/react/webpack.lab-config.js
+++ packages/react/webpack.lab-config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
       },
       {
-        test: /schema\/.*/,
+        test: /pyodide-kernel-extension\/schema\/.*/,
         type: 'asset/resource',
         generator: {
           filename: 'schema/[name][ext][query]',
