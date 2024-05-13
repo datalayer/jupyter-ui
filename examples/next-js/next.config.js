61c8c485969ae8cf199169d6783af97bdad74b01
Eric Charles <eric@datalayer.io>
2024-05-13 12:22:45 +0200

    fix: Next.js example for SVG icon + Link to codesandbox example (#217)
    * fix: next-js example for svg icon + link to codesandbox example
    
    * Automatic application of license header
    
    * docs: cell codesandbox
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/next-js/next.config.js
+++ examples/next-js/next.config.js
@@ -8,34 +8,24 @@
 const nextConfig = {
   reactStrictMode: false,
   webpack: (config, options) => {
-    // https://github.com/vercel/next.js/issues/34501#issuecomment-1046655345
-    // 'filename' is specific to 'asset/resource' type only, but incompatible with 'asset/inline',
-    // see https://webpack.js.org/guides/asset-modules/#custom-output-filename.
-    // Here we rename generator['asset'] into generator['asset/resource'] to avoid conflicts with inline assets.
-    /*
-    if (config.module.generator?.asset?.filename) {
-      if (!config.module.generator['asset/resource']) {
-        config.module.generator['asset/resource'] = config.module.generator.asset
-      }
-      delete config.module.generator.asset
-    }
-    */
     config.module.rules.push(
-      // TODO Fix this @see https://github.com/datalayer/jupyter-ui/issues/104#issuecomment-1906371673
-      // We must escape the JupyterLab theme style sheets to apply specific rules
-      // this is only needed in stand-alone mode
-      /*
-      { 
-        test: /(?<!style\/theme)\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      // Special webpack rule for the JupyterLab theme style sheets.
+      { test: /\.js.map$/, type: 'asset/resource' },
       {
-        test: /style\/theme\.css$/i,
-        loader: 'css-loader',
-        options: { exportType: 'string' },
+        // In .css files, svg is loaded as a data URI.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.css$/,
+        use: {
+          loader: 'svg-url-loader',
+          options: { encoding: 'none', limit: 10000 },
+        },
       },
-      */
+      {
+        // In .ts and .tsx files (both of which compile to .js), svg files
+        // must be loaded as a raw string instead of data URIs.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.js$/,
+        type: 'asset/source',
+      },
       // Ship the JupyterLite service worker.
       {
         resourceQuery: /text/,
