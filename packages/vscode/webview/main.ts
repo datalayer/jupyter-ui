1d30b9ee03e6cc8965017c22d65761981ffd69a2
Eric Charles <eric@datalayer.io>
2025-03-31 15:29:29 +0200

    lint

--- packages/vscode/webview/main.ts
+++ packages/vscode/webview/main.ts
@@ -4,6 +4,9 @@
  * MIT License
  */
 
+import { setStylesTarget } from 'typestyle';
+import { main } from './NotebookVSCode';
+
 /**
  * Returns the nonce used in the page, if any.
  *
@@ -23,15 +26,9 @@ function getNonce() {
 // Xref: https://github.com/styled-components/styled-components/issues/4258#issuecomment-2449562515
 __webpack_nonce__ = getNonce() || '';
 
-import { setStylesTarget } from 'typestyle';
-
 // Fix to apply typestyle styles
 // Xref: https://github.com/typestyle/typestyle/pull/267#issuecomment-390408796
 setStylesTarget(document.querySelector('#typestyle-stylesheet')!);
 
-import { main } from './App';
-
-// Just like a regular webpage we need to wait for the webview
-// DOM to load before we can reference any of the HTML elements
-// or toolkit components
+// Just like a regular webpage we need to wait for the webview DOM to load before we can reference any of the HTML elements or toolkit components
 window.addEventListener('load', main);
