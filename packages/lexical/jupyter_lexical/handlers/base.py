3f6457a1d728c49d91994a2512973543d69dd5c3
Eric Charles <eric@datalayer.io>
2025-04-03 07:09:26 +0200

    chore: python config handlers

---
+++ packages/lexical/jupyter_lexical/handlers/base.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2021-2023 Datalayer, Inc.
+#
+# MIT License
+
+"""Base handler."""
+
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.handler import ExtensionHandlerMixin, ExtensionHandlerJinjaMixin
+
+
+# pylint: disable=W0223
+class BaseTemplateHandler(ExtensionHandlerJinjaMixin, ExtensionHandlerMixin, JupyterHandler):
+    """The Base handler for the templates."""
