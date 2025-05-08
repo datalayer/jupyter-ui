aa6f844c38dbfcc626e2a1ce587b71f90f231a61
Eric Charles <eric@datalayer.io>
2025-05-08 12:05:16 +0200

    fix: config

--- packages/react/src/examples/extensions/cellsidebars/CellSidebarSource.tsx
+++ packages/react/src/examples/extensions/cellsidebars/CellSidebarSource.tsx
@@ -4,22 +4,11 @@
  * MIT License
  */
 
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  ChevronUpIcon,
-  SquareIcon,
-  XIcon,
-} from '@primer/octicons-react';
 import { ActionMenu, Box, Button } from '@primer/react';
-import {
-  NotebookCommandIds
-} from '../../../components/notebook';
+import { ChevronDownIcon, ChevronRightIcon, ChevronUpIcon, SquareIcon, XIcon } from '@primer/octicons-react';
+import { NotebookCommandIds } from '../../../components/notebook';
 import { CellMetadataEditor } from '../../../components/notebook/cell/metadata';
-import {
-  DATALAYER_CELL_SIDEBAR_CLASS_NAME,
-  ICellSidebarProps,
-} from '../../../components/notebook/cell/sidebar';
+import { DATALAYER_CELL_SIDEBAR_CLASS_NAME, ICellSidebarProps } from '../../../components/notebook/cell/sidebar';
 
 export const CellSidebarSource = (props: ICellSidebarProps) => {
   const { commands, model, nbgrader } = props;
