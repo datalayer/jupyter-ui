1e14fc3a94b3079fc1c36896f6eddfa3f4c591d2
Eric Charles <eric@datalayer.io>
2025-09-28 08:39:19 +0200

    fix: build

--- packages/react/src/examples/extensions/celltoolbar/CellToolbarComponent.tsx
+++ packages/react/src/examples/extensions/celltoolbar/CellToolbarComponent.tsx
@@ -5,7 +5,8 @@
  */
 
 import { CodeCell } from '@jupyterlab/cells';
-import { Box, IconButton } from '@primer/react';
+import { IconButton } from '@primer/react';
+import { Box } from '@datalayer/primer-addons';
 import {
   PlayIcon,
   ChevronUpIcon,
@@ -13,7 +14,7 @@ import {
   SquareIcon,
   XIcon,
 } from '@primer/octicons-react';
-import { useNotebookStore2, INotebookExtensionProps } from '../../../components';
+import { useNotebookStore, INotebookExtensionProps } from '../../../components';
 
 type ICellToolbarComponentProps = {
   cell: CodeCell;
@@ -23,7 +24,7 @@ type ICellToolbarComponentProps = {
 export const CellToolbarComponent = (props: ICellToolbarComponentProps) => {
   const { extensionProps } = props;
   const notebookId = extensionProps.notebookId;
-  const notebookStore = useNotebookStore2();
+  const notebookStore = useNotebookStore();
   const activeCell = notebookStore.selectActiveCell(notebookId);
   return activeCell ? (
     <Box
--- packages/react/src/examples/extensions/toc/TocExtension.tsx
+++ packages/react/src/examples/extensions/toc/TocExtension.tsx
@@ -14,7 +14,7 @@ import { BoxPanel } from '@lumino/widgets';
 import {
   NotebookExtension,
   INotebookExtensionProps,
-  notebookStore2,
+  notebookStore,
 } from '../../../components';
 import { JupyterLayoutFactory } from './JupyterLayoutFactory';
 
@@ -87,7 +87,7 @@ export class TocExtension implements NotebookExtension {
       }
 
       if (model) {
-        notebookStore2
+        notebookStore
           .getState()
           .changeTocModel({ id: this._props.notebookId, tocModel: model });
         this._layoutFactory.setModel(model);
