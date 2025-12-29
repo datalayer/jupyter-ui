947efa581da71ad98d2d03612421bdae87714255
Eric Charles <eric@datalayer.io>
2025-12-29 11:43:35 +0100

    fix: build

--- packages/react/src/examples/Bokeh.tsx
+++ packages/react/src/examples/Bokeh.tsx
@@ -8,20 +8,28 @@ import { useMemo } from 'react';
 import { createRoot } from 'react-dom/client';
 import { CellSidebarExtension } from '../components';
 import { Notebook2 } from '../components/notebook/Notebook2';
+import { useJupyter } from '../jupyter';
 import { JupyterReactTheme } from '../theme/JupyterReactTheme';
 import { NotebookToolbar } from './../components/notebook/toolbar/NotebookToolbar';
 
 const BokehExample = () => {
+  const { serviceManager, defaultKernel } = useJupyter({
+    startDefaultKernel: true,
+  });
   const extensions = useMemo(() => [new CellSidebarExtension()], []);
   return (
     <JupyterReactTheme>
-      <Notebook2
-        path="bokeh.ipynb"
-        id="notebook-bokeh-id"
-        height="calc(100vh - 2.6rem)" // (Height - Toolbar Height).
-        extensions={extensions}
-        Toolbar={NotebookToolbar}
-      />
+      {serviceManager && defaultKernel && (
+        <Notebook2
+          path="bokeh.ipynb"
+          id="notebook-bokeh-id"
+          kernel={defaultKernel}
+          serviceManager={serviceManager}
+          height="calc(100vh - 2.6rem)" // (Height - Toolbar Height).
+          extensions={extensions}
+          Toolbar={NotebookToolbar}
+        />
+      )}
     </JupyterReactTheme>
   );
 };
