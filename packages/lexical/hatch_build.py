2551be44763bbef5ad9be4ece44ac59703d769b3
Eric Charles <eric@datalayer.io>
2025-12-29 18:50:56 +0100

    chore: python packages

---
+++ packages/lexical/hatch_build.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2021-2023 Datalayer, Inc.
+#
+# MIT License
+
+import glob
+import os
+
+from subprocess import check_call
+
+import shutil
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def build_javascript():
+    check_call(
+        ['npm', 'install'],
+        cwd=here,
+    )
+    check_call(
+        ['npm', 'run', 'build:webpack', '--mode=production'],
+        cwd=here,
+    )
+    for file in glob.glob(r'./dist/*.*'):
+        shutil.copy(
+            file,
+            './jupyter_lexical/static/'
+        )
+
+
+class JupyterBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        if self.target_name == 'editable':
+            build_javascript()
+        elif self.target_name == 'wheel':
+            build_javascript()
+        elif self.target_name == 'sdist':
+            build_javascript()
