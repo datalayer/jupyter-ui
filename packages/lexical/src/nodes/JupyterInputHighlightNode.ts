88f8dd853892344994c2b5c68905713650bfc0d9
Eric Charles <eric@datalayer.io>
2026-03-06 18:12:15 +0100

    prism component

--- packages/lexical/src/nodes/JupyterInputHighlightNode.ts
+++ packages/lexical/src/nodes/JupyterInputHighlightNode.ts
@@ -15,19 +15,7 @@ import {
   TextNode,
 } from 'lexical';
 
-import * as Prism from 'prismjs';
-
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-markup';
-import 'prismjs/components/prism-markdown';
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-objectivec';
-import 'prismjs/components/prism-sql';
-import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-rust';
-import 'prismjs/components/prism-swift';
+import Prism from '@datalayer/jupyter-react/lib/css/PrismCss';
 
 import {
   addClassNamesToElement,
