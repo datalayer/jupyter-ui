e1f669742a24593ea3ddbf5207141993e5eef186
Eric Charles <eric@datalayer.io>
2026-08-27 10:11:15 +0200

    fix: doc header - fixes #500

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -150,7 +150,7 @@ module.exports = {
             },
             {
               label: 'Bluesky',
-              href: 'https://assets.datalayer.tech/logos-social-grey/youtube.svg',
+              href: 'https://assets.datalayer.tech/logos-social-grey/bluesky.svg',
             },
             {
               label: 'LinkedIn',
